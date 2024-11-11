fa10414c8d6cae2690b57d73d704ea42b789a49a
vRok_Emil <emil.ronnqvist@chasacademy.se>
2024-11-11 15:19:59 +0100

    Emils del

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 const inputName = document.querySelector(".start_input");
 const inputBtn = document.querySelector(".start_button");
 const userForm = document.querySelector(".name_form")
-
+const category = document.querySelector(".allcards")
+const firstSection = document.querySelector(".start_section");
+const categoryBox = document.querySelectorAll(".categorybox")
 
 userForm.addEventListener("submit", (event) => {
     event.preventDefault();
     localStorage.setItem("name", inputName.value);
+    
+    firstSection.style.display = "none"
+    category.style.display = "flex"  
+
+    
 });
+
+categoryBox.forEach((element) => {
+element.addEventListener("click", function (){
+   
+})    
+})
 
