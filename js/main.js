0c28d04495b404eb5ca677829b6ed5a2fb4c0cfa
FilipHans <filip.hansen98@gmail.com>
2024-11-11 16:41:36 +0100

    category and difficulty selection

--- js/main.js
+++ js/main.js
@@ -1,33 +1,106 @@
 const inputName = document.querySelector(".start_input");
 const inputBtn = document.querySelector(".start_button");
+
 const userForm = document.querySelector(".name_form")
 const category = document.querySelector(".allcards")
 const firstSection = document.querySelector(".start_section");
+
 const geographyCategory = document.querySelector(".geography")
 const moviesCategory = document.querySelector(".movies")
 const musicCategory = document.querySelector(".music")
 const randomCategory = document.querySelector(".random")
 
+const easy = document.querySelector('.easy');
+const medium = document.querySelector('.medium');
+const hard = document.querySelector('.hard');
 
-let categorychoice = "";
+const difficultyLevel = document.querySelector('.difficultlevel')
+
+let categoryChoice = "";
+let difficulty = "";
 
 userForm.addEventListener("submit", (event) => {
     event.preventDefault();
     localStorage.setItem("name", inputName.value);
     
-    firstSection.style.display = "none"
-    category.style.display = "flex"  
+    firstSection.style.display = "none";
+    category.style.display = "flex";
 
     
 });
 
+geographyCategory.addEventListener("click",  () =>{
 
-geographyCategory.addEventListener("click", function (event){
-    categorychoice = "1";  
-    console.log(categorychoice); 
+    difficultyLevel.style.display = 'flex';
+    category.style.display = 'none';
+    categoryChoice = "22";  
+    
 })    
 
-moviesCategory.addEventListener("click", function (event){
-    categorychoice = "2";  
-    console.log(categorychoice); 
+moviesCategory.addEventListener("click", () => {
+
+    difficultyLevel.style.display = 'flex';
+    category.style.display = 'none';
+    categoryChoice = "11";  
 })  
+musicCategory.addEventListener("click", () => {
+
+    difficultyLevel.style.display = 'flex';
+    category.style.display = 'none';
+    categoryChoice = "12";  
+})  
+randomCategory.addEventListener("click", () => {
+
+    difficultyLevel.style.display = 'flex';
+    category.style.display = 'none';
+    categoryChoice = "0";  
+})  
+
+easy.addEventListener('click', () => {
+    difficulty = "easy";
+    
+})
+medium.addEventListener('click', () => {
+    difficulty = "medium";
+
+})
+hard.addEventListener('click', () => {
+    difficulty = "hard";
+    console.log(difficulty)
+
+})
+
+async function quizGetter(categoryChoice, difficulty) {
+
+    const quizAPI =  fetch(`https://opentdb.com/api.php?amount=10&category=${categoryChoice}&type=multiple&difficulty=${difficulty}`);
+
+    try {
+        const quiz = await fetch(quizAPI);
+
+        if(!quiz.ok) {
+            throw new Error("HTTP ERROR status: " + quiz.status);
+        }
+        const data = await quiz.json();
+        return data;
+
+    }catch(error){
+        console.error(error);
+    }
+    
+    
+}
+
+
+async function runner(categoryChoice, difficulty ) {
+
+    
+    const data = await quizGetter(categoryChoice,difficulty);
+
+    quizPrinter(data)
+    
+}
+
+function quizDisplay (data){
+
+
+}
