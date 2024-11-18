fc864f7771ef01ea9ed7f3999f2f94c478345f44
Hanna <hannaaosterberg@hotmail.com>
2024-11-18 11:43:22 +0100

    added details

--- js/createBar.js
+++ js/createBar.js
@@ -1,11 +1,13 @@
 const timer = document.querySelector(".timer");
 const timeOut = document.querySelector(".timeOut");
 
+
 export function createBar() {
     timer.innerHTML = `<div class="timer_inside"></div>`;
     setTimeout( () => {
 
         timeOut.textContent = "To slow!";
         console.log("to slow")
+        
     }, 10000)
 }
