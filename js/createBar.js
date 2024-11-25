4c3f47669788c8c159fcc492b688de9af9b56cc9
FilipHans <filip.hansen98@gmail.com>
2024-11-25 13:36:16 +0100

    Fixed loading bar not appearing and disabled button after play again

--- js/createBar.js
+++ js/createBar.js
@@ -2,6 +2,6 @@ const timer = document.querySelector(".timer");
 export const timeOut = document.querySelector(".timeOut");
 
 export function createBar() {
-    timer.innerHTML = `<div class="timer_inside"></div>`;
+    timeOut.innerHTML = `<div class="timer"><div class="timer_inside"></div></div>`;
 
 }
